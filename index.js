0acae8199a472e497764883c9e12babc3cd5af32
Macbook Air <mustafa@Macbooks-MacBook-Air.local>
2024-11-04 16:43:46 +0500

    update

--- index.js
+++ index.js
@@ -21,26 +21,13 @@ app.use(
 
 // ==== CORS Policy ==== //
 
-var whitelist = [
-  "*",
-  "http://127.0.0.1:5500",
-  "https://skilltrack.fun",
-  "https://quiz.codistandemos.org",
-];
-var corsOptions = {
-  origin: function (origin, callback) {
-    console.log("origin+++",origin)
-    if (whitelist.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  allowedHeaders: ["content-type"],
-  credentials: true,
-};
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
+  allowedHeaders: 'Content-Type,Authorization'
+}));
 
-app.use(cors(corsOptions))
+
 
 // ==== Session Configuration ==== //
 app.use(
