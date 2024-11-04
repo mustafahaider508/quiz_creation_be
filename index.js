e1781bf5444b925960dab0e5e24f57205c35aa36
Macbook Air <mustafa@Macbooks-MacBook-Air.local>
2024-11-04 17:03:32 +0500

    update

--- index.js
+++ index.js
@@ -17,32 +17,9 @@ app.use(
   })
 );
 
-
-
 // ==== CORS Policy ==== //
 
-// app.use(cors({
-//   origin: '*',
-//   methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
-//   allowedHeaders: 'Content-Type,Authorization'
-// }));
-
-const whitelist = ['https://skilltrack.fun/']; // List of allowed domains
-const corsOptions = {
-    origin: (origin, callback) => {
-      console.log('origin',origin)
-        if (whitelist.indexOf(origin) !== -1 || !origin) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    credentials: true, // Reflect the requested origin if it's on the whitelist or no origin is provided
-};
-
-app.use(cors(corsOptions));
-
-
+app.use(cors());
 
 // ==== Session Configuration ==== //
 app.use(
@@ -75,5 +52,3 @@ app.use("/api/quiz", quizRouter);
 app.listen(PORT, () => console.log(`Server Started on PORT => ${PORT}`));
 
 export default app;
-
-
