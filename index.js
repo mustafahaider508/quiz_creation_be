46d1645004dc3bcc4778d3d7b25fc73c1a1a80a1
Macbook Air <mustafa@Macbooks-MacBook-Air.local>
2024-11-04 16:49:43 +0500

    update

--- index.js
+++ index.js
@@ -21,11 +21,25 @@ app.use(
 
 // ==== CORS Policy ==== //
 
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
-  allowedHeaders: 'Content-Type,Authorization'
-}));
+// app.use(cors({
+//   origin: '*',
+//   methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
+//   allowedHeaders: 'Content-Type,Authorization'
+// }));
+
+const whitelist = ['*', 'https://skilltrack.fun']; // List of allowed domains
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true, // Reflect the requested origin if it's on the whitelist or no origin is provided
+};
+
+app.use(cors(corsOptions));
 
 
 
